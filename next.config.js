2593f0b51fb64801287c9a46acca21f0c696916e
Sealis04 <suatengco04@gmail.com>
2024-04-28 19:09:02 +0800

    removed setup pages

--- next.config.js
+++ next.config.js
@@ -7,14 +7,14 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 
 const config = {
-    // images:{
-    //     remotePatterns:[{
-    //         protocol:'https',
-    //         hostname:'images.unsplash.com',
-    //         port:'',
-    //         pathname:'/'
-    //     }]
-    // }
+    images:{
+        remotePatterns:[{
+            protocol:'https',
+            hostname:'images.unsplash.com',
+            port:'',
+            pathname:'/'
+        }]
+    }
 };
 
 export default config;
